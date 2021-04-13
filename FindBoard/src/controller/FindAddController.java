fd126d921307a67d095496426590bddac5908fb4
liv660 <73643995+liv660@users.noreply.github.com>
2021-04-13 20:09:26 +0900

    Update FindAddController.java
    오타 수정

--- FindBoard/src/controller/FindAddController.java
+++ FindBoard/src/controller/FindAddController.java
@@ -20,7 +20,7 @@ public class FindAddController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-		req.getRequestDispatcher("/WEB-INF/views/find/add.jsp").forward(req, resp);;
+		req.getRequestDispatcher("/WEB-INF/views/find/add.jsp").forward(req, resp);
 	}
 	
 	@Override
