5824d017306697e0b64ddce21b958972ba3429eb
liv660 <73643995+liv660@users.noreply.github.com>
2021-04-13 20:12:16 +0900

    Update FindBoardDao.java
    주석 수정

--- FindBoard/src/dao/face/FindBoardDao.java
+++ FindBoard/src/dao/face/FindBoardDao.java
@@ -10,7 +10,7 @@ public interface FindBoardDao {
 	/**
 	 * FindBoard 테이블 시퀀스의 nextval을 조회한다.
 	 * @param conn	DB 연결 객체
-	 * @return		findboard_seq.nextval
+	 * @return	findboard_seq.nextval
 	 */
 	int selectFindno(Connection conn);
 
@@ -19,7 +19,7 @@ public interface FindBoardDao {
 	 * 새 게시글의 데이터를 FindBoard 테이블에 삽입한다.
 	 * @param conn		DB 연결 객체
 	 * @param findBoard	새 게시글 데이터가 담긴 전달 파라미터
-	 * @return			삽입된 행의 수
+	 * @return		삽입된 행의 수
 	 */
 	int insert(Connection conn, FindBoard findBoard);
 
@@ -28,7 +28,7 @@ public interface FindBoardDao {
 	 * 새 게시글의 첨부파일을 FingImg 테이블에 삽입한다.
 	 * @param conn		DB 연결 객체
 	 * @param findImg	첨부파일 데이터가 담긴 전달 파라미터
-	 * @return			삽입된 행의 수
+	 * @return		삽입된 행의 수
 	 */
 	int insertImg(Connection conn, FindImg findImg);
 
