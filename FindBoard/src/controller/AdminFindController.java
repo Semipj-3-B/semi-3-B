76156e16fdc3234a40d3a94f0d36983d19dfc505
Olivia <liv660n@gmail.com>
2021-04-23 09:25:00 +0900

    관리자 페이지 - 찾기 게시글 관리
    불필요한 코드 삭제

--- FindBoard/src/controller/AdminFindController.java
+++ FindBoard/src/controller/AdminFindController.java
@@ -10,8 +10,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.gson.Gson;
-
 import dto.FindBoard;
 import dto.Usertb;
 import service.face.AdminService;
@@ -28,19 +26,10 @@ public class AdminFindController extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		AdminPaging apaging = adminService.getPaging(req);
 		List<FindBoard> findList = adminService.getFindList(apaging);
-		
+
 		req.setAttribute("findList", findList);
 		req.setAttribute("apaging", apaging);
-		
 		req.getRequestDispatcher("/WEB-INF/views/admin/find.jsp").forward(req, resp);
-		
-//		req.setCharacterEncoding("UTF-8");
-//		AdminPaging apaging = adminService.getPaging(req);
-//		List<FindBoard> findList = adminService.getFindList(apaging);
-//		Gson gson = new Gson();
-//		resp.setContentType("application/json; charset=utf-8");
-//		PrintWriter out = resp.getWriter();
-//		out.print(gson.toJson(findList));
 	}
 
 	@Override
